a2eb9aa2fa07facba183755d16e467106be9b265
zacallen25 <zacharys.allen@calbaptist.edu>
2020-10-29 16:49:25 -0700

    I have started work on the spells, but not done.

---
+++ src/Effects.java
@@ -0,0 +1,4 @@
+public enum Effects {
+    FIRE, FROZEN;
+
+}
--- src/Spell.java
+++ src/Spell.java
@@ -1,4 +1,55 @@
 public interface Spell {
-    public int getDamage();
-    public int rollDie();
+    public DiceTray rand = new DiceTray();
+    public int rollHit();
+    public int rollDieDamage();
+    public Effects effect();
+
+    class Fire_Blast implements Spell {
+        //Unlimited uses
+        public int rollHit() {
+            return rand.d20();
+        }
+
+        public int rollDieDamage() {
+            return rand.d6();
+        }
+
+        public Effects effect() {
+            return Effects.FIRE;
+        }
+    }
+
+    class Ice_Blast implements Spell {
+        //Unlimited uses
+        public int rollHit() {
+            return rand.d20();
+        }
+        public int rollDieDamage() {
+            return rand.d6();
+        }
+        public Effects effect() {
+            return Effects.FROZEN;
+        }
+    }
+
+    class Fire_Stream implements Spell {
+        int uses = 2;
+        public int rollHit() {
+            if (uses <= 0) {
+                return 0;
+            }
+            uses--;
+            return rand.d20();
+        }
+        public int rollDieDamage() {
+            return rand.d12();
+        }
+        public Effects effect() {
+            return Effects.FIRE;
+        }
+    }
+
+
+
+
 }
